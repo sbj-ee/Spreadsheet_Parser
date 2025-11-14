fb7ed64c4addd1edad8656c7af438fbfc8098660
Stephen B. Johnson <49662809+sbj-ee@users.noreply.github.com>
2025-11-14 10:12:17 -0600

    Configure numpy logging and warnings
    Added logging and warning configurations for numpy.

--- example1.py
+++ example1.py
@@ -1,6 +1,19 @@
 import polars as pl
 import pandas as pd
+import numpy as np
+import warnings, os, logging
+logging.getLogger("numpy").setLevel(logging.ERROR)
+warnings.filterwarnings("ignore")
+os.environ["NUMPY_WARN_IF_NO_FALLBACK"] = "0"
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=UserWarning, module="numpy")
+warnings.filterwarnings("ignore", message="Signature.*longdouble.*", module="numpy")
+warnings.filterwarnings("ignore", category=UserWarning, module=np.__name__)
+from pprint import pprint
 
-excel_file = "your_workbook.xlsx"
+pl.Config.set_tbl_rows(-1)
+excel_file = "/home/usrsbj/EQUINIX.xlsx"
 xls = pd.ExcelFile(excel_file)
 dfs = {sheet: pl.from_pandas(pd.read_excel(xls, sheet_name=sheet)) for sheet in xls.sheet_names}
+
+pprint(dfs)
